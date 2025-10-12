e461a24fe96ac29ce0610d45ab33334a770d898d
Tymur-Tykva <tykva.tymur@gmail.com>
2025-10-12 15:47:25 +0200

    Created test UI layout

--- Frame.java
+++ Frame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JFrame {
     private Panel panel;
@@ -10,7 +11,6 @@ public class Frame extends JFrame {
     public Frame() {
         // Set the frame's parameters.
         super("SOME TITLE");
-        setSize(600, 400); // TODO: Change to be dynamically calculated from the panel size.
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -22,6 +22,20 @@ public class Frame extends JFrame {
      * Initialise the UI layout.
      */
     public void init() {
+        setSize(600, 400); // TODO: Change to be dynamically calculated from the panel size.
+        setLayout(new GridLayout(3, 1));
 
+        // Create test labels to test the layout.
+        JLabel label1 = new JLabel("Top label", JLabel.CENTER);
+        label1.setSize(600, 50);
+        JLabel label2 = new JLabel("Bottom label", JLabel.CENTER);
+        label2.setSize(600, 50);
+
+        // Add elements to the frame.
+        add(label1);
+        add(panel);
+        add(label2);
+
+        setVisible(true);
     }
 }
--- Panel.java
+++ Panel.java
@@ -1,5 +1,10 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
-public class Panel {
-
+public class Panel extends JPanel {
+    public Panel() {
+        setSize(600, 300);
+        setBackground(Color.CYAN);
+    }
 }
