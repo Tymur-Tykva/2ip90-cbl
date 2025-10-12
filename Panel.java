81095f4a55a0e495a970fa488afb3a0052a0b749
Grebenarov123 <borislavgg5@gmail.com>
2025-10-12 17:33:52 +0200

    Color Change

--- Panel.java
+++ Panel.java
@@ -4,6 +4,6 @@ import javax.swing.*;
 public class Panel extends JPanel {
     public Panel() {
         setPreferredSize(new Dimension(600, 400));
-        setBackground(Color.CYAN);
+        setBackground(new Color(138, 233, 131));
     }
 }
