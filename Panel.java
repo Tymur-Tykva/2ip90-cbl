572484a184d3a68b648be8c256c1321f6d7eb926
Grebenarov123 <borislavgg5@gmail.com>
2025-10-19 00:25:20 +0200

    Fixed the array list and the method to print out the apples

--- Panel.java
+++ Panel.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Panel extends JPanel {
@@ -13,57 +14,39 @@ public class Panel extends JPanel {
         // set the forloop for the checkpatter
         int sidePanel = 605;
         int sqr = 605 / 11;
-        int x = 0;
-        int y = 0;
         for (int col = 0; col < 11; col++) {
             for (int row = 0; row < 11; row++) {
                 if (((row % 2 == 0) && (col % 2 == 0)) || (!(row % 2 == 0) && !(col % 2 == 0))) {
                     g2D.setColor(new Color(138, 233, 131));
-                    g2D.fillRect(x + (row * sqr), y + (col * sqr), 55, 55);
+                    g2D.fillRect(row * sqr, col * sqr, 55, 55);
                 } else {
                     g2D.setColor(new Color(101, 196, 99));
-                    g2D.fillRect(x + (row * sqr), y + (col * sqr), 55, 55);
+                    g2D.fillRect(row * sqr, col * sqr, 55, 55);
                 }
 
             }
 
         }
 
-        // set a 2d array for containing apples
-        int[][] applesArray = new int[11][11];
-        applesArray[0][0] = 0;
-        applesArray[1][0] = 1;
-        applesArray[2][0] = 2;
-        applesArray[3][0] = 3;
-
-        // draw the red apples according to the 2d array
+        //set a 2d array list for containing apples
+        ArrayList<Point> appleLocations = new ArrayList<>();
         int sideApple = 45;
         int padding = 5;
 
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
-                // cases for each apple
-                switch (applesArray[i][j]) {
-                    // Red apples == 1
-                    case 1 -> {
-                        g2D.setColor(Color.RED);
-                        g2D.fillOval(i * sqr + padding, j * sqr + padding, sideApple, sideApple);
-                    }
-                    // Black apples == 2
-                    case 2 -> {
-                        g2D.setColor(Color.BLACK);
-                        g2D.fillOval(i * sqr + padding, j * sqr + padding, sideApple, sideApple);
-                    }
-                    // Yellow apples == 3
-                    case 3 -> {
-                        g2D.setColor(Color.YELLOW);
-                        g2D.fillOval(i * sqr + padding, j * sqr + padding, sideApple, sideApple);
-                    }
-                    // Empty square == 0
-                    default -> {
-                    }
-                }
-            }
+        //set initial red apple
+        appleLocations.add(new Point(7,5));
+
+        //initialize the colors of the different apples
+        Color red = new Color(255, 59, 59);
+        Color black = new Color(64, 64, 64);
+        Color yellow = new Color(236, 255, 54);
+
+        //print out all of the apples from the array list
+        for (Point point : appleLocations) {
+            Color appleColor = red;
+            g2D.setColor(appleColor);
+            g2D.fillOval(point.x * sqr + padding, point.y * sqr + padding, sideApple, sideApple);
         }
+
     }
 }
