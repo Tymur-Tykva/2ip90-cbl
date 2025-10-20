3c15e34253c4d343cb0f5480049b46d14002f15a
Grebenarov123 <borislavgg5@gmail.com>
2025-10-20 17:38:18 +0200

    Fixed initial snake position

--- utils/Config.java
+++ utils/Config.java
@@ -8,12 +8,12 @@ public class Config {
     public static final int GRID_WIDTH = 11;
     public static final int GRID_HEIGHT = 11;
     // Snake
-    public static final Point INITIAL_SNAKE_POSITION = new Point(4, 4);
+    public static final Point INITIAL_SNAKE_POSITION = new Point(4, 5);
     public static final int INITIAL_SNAKE_LENGTH = 3;
     // Apples
     public static final ArrayList<Point> INITIAL_APPLE_POSITIONS = new ArrayList<Point>() {
         {
-            add(new Point(6, 4));
+            add(new Point(6, 5));
         }
     };
 }
