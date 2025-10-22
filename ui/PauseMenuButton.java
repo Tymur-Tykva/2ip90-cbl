21599f91f5991d01197e798287e3244d84b3221d
Grebenarov123 <borislavgg5@gmail.com>
2025-10-22 17:56:29 +0200

    Changed colors

--- ui/PauseMenuButton.java
+++ ui/PauseMenuButton.java
@@ -13,14 +13,14 @@ public class PauseMenuButton extends JButton {
         // Intiliaze the customs for the buttons
         Font fontButton = new Font("Serif", Font.BOLD, 25);
         Color colorButton = new Color(200, 163, 117);
-        Color colorTextButton = new Color(101, 67, 33);
+        Color colorBrown = new Color(101, 67, 33);
         LineBorder thickBorder = new LineBorder(new Color(101, 67, 33), 3, false);
         EmptyBorder paddingBorder = new EmptyBorder(7, 10, 7, 10);
         CompoundBorder compoundBorder = new CompoundBorder(thickBorder, paddingBorder);
 
         setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         setFont(fontButton);
-        setForeground(colorTextButton);
+        setForeground(colorBrown);
         setBackground(colorButton);
         setBorder(compoundBorder);
         setPreferredSize(new Dimension(180, 50));
