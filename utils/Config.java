568a43172e31b01017ad39929ffb9eb7f75a84cf
Grebenarov123 <borislavgg5@gmail.com>
2025-10-23 12:06:28 +0200

    Temporary apple

--- utils/Config.java
+++ utils/Config.java
@@ -14,6 +14,7 @@ public class Config {
     public static final ArrayList<Point> INITIAL_APPLE_POSITIONS = new ArrayList<Point>() {
         {
             add(new Point(6, 5));
+            add(new Point(6, 4));
         }
     };
 }
